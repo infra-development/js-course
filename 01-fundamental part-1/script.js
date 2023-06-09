9d079f222e5576c5ce1fc250252e8935aeaeaf2c
kaushik <kaushikbchaudhary@gmail.com>
2023-06-09 15:24:56 +0530

    JS-1 values and variables

---
+++ 01-fundamental part-1/script.js
@@ -0,0 +1,25 @@
+// values and variable
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+console.log("kaushik");
+console.log(23);
+
+// this are some convention have to write code
+let firstName = "kaushik";
+let first = "haresh";
+let firstNamePerson;
+let first_name_person;
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// let 3years = 3; // Error: unvalid token
+// variable name can't start with numbers
+
+let job1 = "Programmer";
+let job2 = "teacher";
+
+// this set of variable more readable
+let myFirstJob = "Programmer";
+let myCurrentJob = "teacher";
