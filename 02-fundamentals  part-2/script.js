7bcbe4dd525c8bba62f910293c63046d82f544ad
kaushik <kaushikbchaudhary@gmail.com>
2023-06-11 20:13:22 +0530

    JS-20 Function Declarations vs. Expressions

--- 02-fundamentals  part-2/script.js
+++ 02-fundamentals  part-2/script.js
@@ -7,3 +7,16 @@ if (hasDriversLicense) console.log("I can drive ::D");
 console.log(hasDriversLicense);
 // const interface = "Audio";
 // const private = 333;
+
+// function declaration
+function calcAge1(birthYear) {
+  return 2023 - birthYear;
+}
+
+const age1 = calcAge1(2000);
+// function expression
+const calcAge2 = function (birthYear) {
+  return 2023 - birthYear;
+};
+const age2 = calcAge2(1972);
+console.log(age1, age2);
