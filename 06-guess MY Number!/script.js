d7c5dfe01f1b30069c7168108ec532624ac4012f
kaushik <kaushikbchaudhary@gmail.com>
2023-06-14 03:44:43 +0530

    JS-40 Selecting and Manipulating Elements

--- 06-guess MY Number!/script.js
+++ 06-guess MY Number!/script.js
@@ -1,3 +1,10 @@
 "use strict";
 
 console.log(document.querySelector(".message").textContent);
+document.querySelector(".message").textContent = "Correct Number!";
+
+document.querySelector(".number").textContent = 20;
+document.querySelector(".score").textContent = 10;
+
+document.querySelector(".guess").value = 20;
+console.log(document.querySelector(".guess").value);
