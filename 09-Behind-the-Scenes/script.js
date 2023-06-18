d2a0a15c1449162a877588d5eb904ddd4a6421c6
kaushik <kaushikbchaudhary@gmail.com>
2023-06-18 21:03:00 +0530

    JS-48 Primitives vs. Objects in Practice

--- 09-Behind-the-Scenes/script.js
+++ 09-Behind-the-Scenes/script.js
@@ -140,3 +140,35 @@ var addArrow = (a, b) => {
   return a + b;
 };
 addArrow(2, 5, 8);
+
+let lastName = "williams";
+let oldLastName = lastName;
+lastName = "Davis";
+console.log(lastName, oldLastName);
+
+const jessica = {
+  firstName: "Jessica",
+  lastName: "Williams",
+  age: 27,
+};
+
+const marriedJessica = jessica;
+marriedJessica.lastName = "Davis";
+console.log("Before marriage", jessica);
+console.log("After marriage", marriedJessica);
+
+const jessica2 = {
+  firstName: "Jessica",
+  lastName: "Williams",
+  age: 27,
+  family: ["Alice", "Bob"],
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = "Davis";
+
+jessicaCopy.family.push("Marry");
+jessicaCopy.family.push("John");
+
+console.log("Before marriage", jessica2);
+console.log("After marriage", jessicaCopy);
