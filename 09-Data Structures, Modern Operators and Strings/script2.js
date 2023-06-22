bc8400c3145e215c4b615392765b5e7ba3cbe068
kaushik <kaushikbchaudhary@gmail.com>
2023-06-22 17:27:48 +0530

    JS-63 Maps iteration

--- 09-Data Structures, Modern Operators and Strings/script2.js
+++ 09-Data Structures, Modern Operators and Strings/script2.js
@@ -245,3 +245,35 @@ console.log(rest.get(arr));
 rest.set(document.querySelector("h1"), "Heading");
 console.log(rest);
 console.log(rest.size);
+
+const question = new Map([
+  ["question", "What is the best programming language in the world?"],
+  [1, "c"],
+  [2, "Java"],
+  [3, "javaScript"],
+  ["correct", 3],
+  [true, "Correct"],
+  [false, "Try again!"],
+]);
+console.log(question);
+
+// Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// Quiz app
+console.log(question.get("question"));
+for (const [key, value] of question) {
+  if (typeof key === "number") console.log(`Answer ${key}: ${value}`);
+}
+// const answer = Number(prompt(`Your answer`));
+const answer = 3;
+console.log(answer);
+console.log(question.get(answer === question.get("correct")));
+
+// Convert map to array
+console.log([...question]);
+// console.log(question.entries());
+console.log([...question.keys()]);
+console.log([...question.values()]);
