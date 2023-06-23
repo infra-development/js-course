06af7d376183bfbfe083e1821a3c9cef412e1ce3
kaushik <kaushikbchaudhary@gmail.com>
2023-06-23 14:45:26 +0530

    JS-65 Working With Strings-Part 1

--- 09-Data Structures, Modern Operators and Strings/script2.js
+++ 09-Data Structures, Modern Operators and Strings/script2.js
@@ -326,3 +326,38 @@ for (const [min, event] of gameEvents) {
   const half = min < 45 ? "FIRST" : "SECOND";
   console.log(`[${half} HALF] ${min}: ${event}`);
 }
+
+const airline = "TAP Air Portugal";
+const plane = "A320";
+
+console.log(plane[0]);
+console.log(airline.length);
+console.log("B737".length);
+
+console.log(airline.indexOf("r"));
+console.log(airline.lastIndexOf("r"));
+console.log(airline.indexOf("portugal"));
+
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+
+console.log(airline.slice(0, airline.indexOf(" ")));
+console.log(airline.slice(airline.lastIndexOf(" ") + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+  // B and E are middle seats
+  const s = seat.slice(-1);
+  if (s === "B" || s === "E") console.log("You got the middle seat");
+  else console.log("You got lucky");
+};
+checkMiddleSeat("11B");
+checkMiddleSeat("23C");
+checkMiddleSeat("3E");
+// Behind the scene js automatically convert to primitive type to object
+console.log(new String("kaushik"));
+console.log(typeof new String("Kaushik"));
+console.log(typeof new String("Kaushik").slice(1));
+console.log(new String("Kaushik").slice(1));
