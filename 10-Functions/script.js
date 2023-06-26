4c9e3a0e57d99cff24c2a7168f92963354fdcadf
kaushik <kaushikbchaudhary@gmail.com>
2023-06-26 19:24:15 +0530

    JS-72 Functions Returning Functions

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -79,3 +79,16 @@ const high5 = function () {
 
 document.body.addEventListener("click", high5);
 ["Jonas", "Martha", "Adam"].forEach(high5);
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+const greeterHey = greet("Hey");
+greeterHey("Kaushik");
+greeterHey("Steven");
+greet("Hello")("Kaushik");
+// Challenge
+const greetArrow = (greeting) => (name) => console.log(`${greeting} ${name}`);
+greetArrow("Hi")("Kaushik");
