2b5ea37e2a98c98563aa7bc23819e24ca72d34bc
kaushik_jayshreeram <kaushikbchaudhary@gmail.com>
2023-07-03 17:06:24 +0530

    JS-83 creating DOM Elements

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -1,7 +1,5 @@
 "use strict";
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
 // BANKIST APP
 
 // Data
@@ -60,6 +58,24 @@ const inputTransferAmount = document.querySelector(".form__input--amount");
 const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
+
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = "";
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? "deposit" : "withdrawal";
+
+    const html = `
+     <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+          <div class="movements__value">${mov}€</div>
+        </div>
+    `;
+    containerMovements.insertAdjacentHTML("afterbegin", html);
+  });
+};
+displayMovements(account1.movements);
 
 // Simply Array Methods
 let arr = ["a", "b", "c", "d", "e"];
