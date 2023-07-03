d55b6aae0c1b6b3864940e24be135c82ee9edf44
kaushik_jayshreeram <kaushikbchaudhary@gmail.com>
2023-07-03 21:48:59 +0530

    JS-85 The map Method

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -186,3 +186,28 @@ function checkDogs(dogsJulia, dogsKate) {
 
 checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+
+const eurToUsd = 1.1;
+// const movementsUSD1 = movements.map(function (mov) {
+//   return mov * eurToUsd;
+// });
+// console.log(movementsUSD1);
+
+const movementsUSD = movements.map((mov) => mov * eurToUsd);
+console.log(movementsUSD);
+
+const movementsUSDfor = [];
+for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+console.log("movements for:", movementsUSDfor);
+
+const movementsDescriptions = movements.map((mov, i, arr) => {
+  // if (mov < 0) {
+  //   return `Movement ${i + 1}: You withdraw ${Math.abs(mov)}`;
+  // } else {
+  //   return `Movement ${i + 1}: You deposited ${mov}`;
+  // }
+  return `Movement ${i + 1}: You ${
+    mov < 0 ? "withdraw" : "deposited"
+  } ${Math.abs(mov)} `;
+});
+console.log(movementsDescriptions.join("\n"));
