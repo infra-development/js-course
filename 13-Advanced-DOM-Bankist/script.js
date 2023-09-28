be9c0352c0a1d44a294310672c70d31172354e83
kaushik_jayshreeram <kaushikbchaudhary@gmail.com>
2023-09-28 12:33:53 +0530

    JS-121 Passing Arguments to Event Handlers

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -113,8 +113,27 @@ tabContainer.addEventListener('click', function (e) {
   document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
 
 
-})
+});
 
+const nav = document.querySelector('.nav');
+
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelectorAll('img');
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+
+    logo.forEach(el => {
+      el.style.opacity = this;
+    });
+  };
+};
+
+nav.addEventListener('mouseover',handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
 
 ////////////////////////////////
 ////////////////////////////////
