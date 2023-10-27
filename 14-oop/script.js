1c714f4b96027f54190d256a50d07c082d843056
kaushik_jayshreeram <kaushikbchaudhary@gmail.com>
2023-10-27 13:33:50 +0530

    Js-131 Prototypes

--- 14-oop/script.js
+++ 14-oop/script.js
@@ -5,7 +5,7 @@ const Person = function (firstName, birthYear) {
     this.birthYear = birthYear;
     // Never do this
     // this.calcAge = function () {
-    //     console.log(2037-birthYear);
+    //     console.log(2037- this.birthYear);
     // }
 };
 
@@ -22,3 +22,25 @@ const haresh = new Person('Haresh', 1989);
 console.log(manish, haresh);
 
 console.log(manish instanceof Person);
+
+Person.prototype.calcAge = function () {
+    console.log(2023 - this.birthYear);
+};
+
+haresh.calcAge();
+manish.calcAge();
+
+console.log(haresh.__proto__);
+console.log(manish.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(haresh));
+console.log(Person.prototype.isPrototypeOf(kaushik));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+// .prototypeOfLinkedObjects
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(kaushik.species , manish.species);
+
+console.log(kaushik.hasOwnProperty('firstName'));
+console.log(kaushik.hasOwnProperty('species'));
