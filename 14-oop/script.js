0d91b1fd62982179d4f954cc5d6cb20e2db397af
kaushik_jayshreeram <kaushikbchaudhary@gmail.com>
2023-10-30 18:16:27 +0530

    JS-132 Prototypal Inheritance on Built-In Objects

--- 14-oop/script.js
+++ 14-oop/script.js
@@ -44,3 +44,23 @@ console.log(kaushik.species , manish.species);
 
 console.log(kaushik.hasOwnProperty('firstName'));
 console.log(kaushik.hasOwnProperty('species'));
+
+console.log(kaushik.__proto__);
+console.log(kaushik.__proto__.__proto__);
+console.log(kaushik.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 6, 5, 6, 9, 9];  // new Array === []
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+    return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(x => x + 1);
