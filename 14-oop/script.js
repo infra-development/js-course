92272838cbd0ddd055fbbf462804510b3ec68181
kaushik_jayshreeram <kaushikbchaudhary@gmail.com>
2023-10-31 11:56:03 +0530

    js-133 Coding Challenge #1

--- 14-oop/script.js
+++ 14-oop/script.js
@@ -64,3 +64,42 @@ console.log(arr.unique());
 
 const h1 = document.querySelector('h1');
 console.dir(x => x + 1);
+
+// Coding Challenge #1
+// Your tasks:
+// 1. Use a constructor function to implement a 'Car'. A car has a 'make' and a
+// 'speed' property. The 'speed' property is the current speed of the car in
+// km/h
+// 2. Implement an 'accelerate' method that will increase the car's speed by 10,
+// and log the new speed to the console
+// 3. Implement a 'brake' method that will decrease the car's speed by 5, and log
+// the new speed to the console
+// 4. Create 2 'Car' objects and experiment with calling 'accelerate' and
+// 'brake' multiple times on each of them
+// Test data:
+// §
+// §
+// Data car 1: 'BMW' going at 120 km/h
+// Data car 2: 'Mercedes' going at 95 km/h
+// GOOD LUCK 😀
+
+const Car = function (make, speed) {
+    this.speed = speed;
+    this.make = make;
+}
+
+Car.prototype.accelerate = function () {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed}`);
+};
+Car.prototype.brake = function () {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed}`);
+};
+const bmw = new Car('BMW',120);
+const mercedes = new Car('MERCEDES', 95);
+
+bmw.accelerate();
+bmw.accelerate();
+bmw.brake();
+bmw.accelerate();
