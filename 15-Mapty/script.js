07ef60b40b28a8539faff520827f553de132324e
kaushik_jayshreeram <kaushikbchaudhary@gmail.com>
2023-11-21 16:01:11 +0530

    JS-150 Mapty

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -27,9 +27,23 @@ if (navigator.geolocation) {
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             }).addTo(map);
 
-            L.marker(coords).addTo(map)
-                .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-                .openPopup();
+            map.on('click', function (mapEvent) {
+                console.log(mapEvent);
+                const { lat, lng } = mapEvent.latlng;
+                console.log(lat,lng);
+
+                L.marker([lat,lng]).addTo(map)
+                    .bindPopup(L.popup({
+                        maxWidth: 250,
+                        minWidth: 100,
+                        autoClose: false,
+                        closeOnClick: false,
+                        className: 'running-popup',
+                        // content:"Hello world",
+                    }))
+                .setPopupContent('Workout')
+                .openPopup();   
+            })
     }, function () {
         alert('Could not get your position')
     })
